e86a9d127c9ac1e87294a2397a9a827dfce032af
Ronny700 <62081964+Ronny700@users.noreply.github.com>
2024-11-23 17:01:35 -0300

    Add files via upload

---
+++ Aula_5.py
@@ -0,0 +1,46 @@
+print("Bem-vindo")
+
+variavel_boolean = True
+print("Tipo de variavel_boolean:", type(variavel_boolean))
+
+numero_decimal1 = 5.4
+numero_decimal2 = 3.2
+resultado_multiplicacao = numero_decimal1 * numero_decimal2
+print("Resultado da multiplicação de dois números decimais:", resultado_multiplicacao)
+
+numero_inteiro1 = 10
+numero_inteiro2 = 3
+resultado_divisao = numero_inteiro1 / numero_inteiro2
+print("Resultado da divisão de dois números inteiros:", resultado_divisao)
+
+
+numero_inteiro3 = 15
+numero_inteiro4 = 8
+resultado_subtracao = numero_inteiro3 - numero_inteiro4
+print("Resultado da subtração de dois números inteiros:", resultado_subtracao)
+
+resultado_divisao_inteira = numero_inteiro1 // numero_inteiro2
+print("Resultado da divisão inteira de dois números inteiros:", resultado_divisao_inteira)
+
+numero_decimal3 = 2.5
+numero_decimal4 = 1.6
+resultado_multiplicacao_4 = numero_decimal1 * numero_decimal2 * numero_decimal3 * numero_decimal4
+print("Resultado da multiplicação de 4 números decimais:", resultado_multiplicacao_4)
+
+numero = 7
+dobro_numero = numero * 2
+print("O dobro do número", numero, "é:", dobro_numero)
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+soma = num1 + num2
+print("A soma dos dois números é:", soma)
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+email = input("Digite seu e-mail: ")
+
+print("\nCadastro realizado com sucesso!")
+print("Nome:", nome)
+print("Idade:", idade)
+print("E-mail:", email)
